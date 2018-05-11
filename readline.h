34e382c9a07eda364989fb101847ae18ad4eb120
lcadenas <32308123+lcadenas@users.noreply.github.com>
2018-05-10 22:59:39 -0400

    Create readline.h

---
+++ readline.h
@@ -0,0 +1,7 @@
+//This is header file that contains the prototype for the read_line function. Laura Cadenas Sainz
+#ifndef READLINE_H
+#define READLINE_H
+
+int read_line(char str[], int n);
+
+#endif
